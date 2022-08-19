d88a09f6fe9b51086d75692d792a694d59282488
Torbjørn Hallenberg <torbjorn.hallenberg@nav.no>
2022-08-19 18:05:49 +0200

    mooved webloc to root dir

--- scripts/nav-webapps.sh
+++ scripts/nav-webapps.sh
@@ -9,7 +9,7 @@ are granted access to many of the apps that are featured
 there.
 WEBAPPS
 
-mv stuff/nav-webapps.webloc /Users/"$user"/Documents/nav-webapps.webloc
+mv nav-webapps.webloc /Users/"$user"/Documents/nav-webapps.webloc
 
 defaults write com.apple.dock persistent-others -array-add \
 "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>file:////Users/"$user"/Documents/nav-webapps.webloc</string><key>_CFURLStringType</key><integer>15</integer></dict><key>file-type</key><integer>32</integer></dict><key>tile-type</key><string>file-tile</string></dict>"
