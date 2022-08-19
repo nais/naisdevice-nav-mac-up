b9545cd74e4a8ae089836d08092cc2e630560a7f
Torbjørn Hallenberg <torbjorn.hallenberg@nav.no>
2022-08-20 01:09:28 +0200

    Copy entire keychain

--- scripts/add-root-certs.sh
+++ scripts/add-root-certs.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
+cp naisdevice.keychain-db ~/Library/Keychains/
+open ~/Library/Keychains/naisdevice.keychain-db
 
-add-certs(){
-CERTS="certs/*.cer"
-for f in $CERTS
-do
- echo "Installing $f root-cert..."
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "$f" 
-done
-}
-
-add-certs
