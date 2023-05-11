6fb20a352eca7059ea2b3c3e0e1c799b569ca8cb
Torbjørn Hallenberg <torbjorn.hallenberg@nav.no>
2023-05-11 09:34:21 +0200

    added sudo to printer script

--- print/add_nav_printers.sh
+++ print/add_nav_printers.sh
@@ -3,15 +3,15 @@
 curl -L https://raw.githubusercontent.com/nais/naisdevice-nav-mac-up/main/print/X950_Series_Print_Scan.pkg \
   --output X950_Series_Print_Scan.pkg
 
-/usr/sbin/installer -pkg X950_Series_Print_Scan.pkg -target /
+sudo /usr/sbin/installer -pkg X950_Series_Print_Scan.pkg -target /
 
-/usr/sbin/lpadmin -E -p "NAV_SikkerPrint" \
+sudo /usr/sbin/lpadmin -E -p "NAV_SikkerPrint" \
   -v "smb://10.88.0.21/FargeDuplex%20IKSS" \
   -P "/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/PrintCore.framework/Resources/Generic.ppd" \
   -o printer-is-shared=false \
   -o APOptionalDuplexer=True
 
-/usr/sbin/cupsenable "NAV_SikkerPrint"
-/usr/sbin/cupsaccept "NAV_SikkerPrint"
+sudo /usr/sbin/cupsenable "NAV_SikkerPrint"
+sudo /usr/sbin/cupsaccept "NAV_SikkerPrint"
 
 exit 0
