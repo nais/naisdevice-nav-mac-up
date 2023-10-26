2ca7d9d9c91d94e65944bfffed4844c97979771d
Torbjørn Hallenberg <torbjorn.hallenberg@nav.no>
2023-10-26 10:32:04 +0200

    Changed print url to DNS for Mac

--- print/add_nav_printers.sh
+++ print/add_nav_printers.sh
@@ -3,22 +3,22 @@
 kernel_name="$(uname -s)"
 
 case "${kernel_name}" in
-  Linux*)
-    warning="
+Linux*)
+  warning="
       This requires that smbclient is installed.
       smbclient can be installed on the command line like so:
 
       'sudo apt install smbclient'
     "
-    ;;
-  Darwin*)
-    warning="
+  ;;
+Darwin*)
+  warning="
       This requires Rosetta2 so the installer might fail.
       Rosetta2 can be installed on the command line like so:
 
       '/usr/sbin/softwareupdate --install-rosetta --agree-to-license'
       "
-    ;;
+  ;;
 esac
 
 cat <<EOF
@@ -36,46 +36,59 @@ echo # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 
   case "${kernel_name}" in
-    Linux*)
-      echo "Downloading Lexmark X950 series PPD"
-      curl -L "https://www.openprinting.org/download/PPD/Lexmark/Lexmark_X950_Series.ppd" \
-        -o postscript-lexmark-x950.ppd
-      sudo mkdir -p /usr/local/share/ppd/
-      sudo mv postscript-lexmark-x950.ppd /usr/local/share/ppd/
-      ppd_path=/usr/local/share/ppd/postscript-lexmark-x950.ppd
-      ;;
-    Darwin*)
-      curl -L https://raw.githubusercontent.com/nais/naisdevice-nav-mac-up/main/print/X950_Series_Print_Scan.pkg \
-        -o X950_Series_Print_Scan.pkg
+  Linux*)
+    echo "Downloading Lexmark X950 series PPD"
+    curl -L "https://www.openprinting.org/download/PPD/Lexmark/Lexmark_X950_Series.ppd" \
+      -o postscript-lexmark-x950.ppd
+    sudo mkdir -p /usr/local/share/ppd/
+    sudo mv postscript-lexmark-x950.ppd /usr/local/share/ppd/
+    ppd_path=/usr/local/share/ppd/postscript-lexmark-x950.ppd
+    ;;
+  Darwin*)
+    curl -L https://raw.githubusercontent.com/nais/naisdevice-nav-mac-up/main/print/X950_Series_Print_Scan.pkg \
+      -o X950_Series_Print_Scan.pkg
 
-      echo "Run Lexmark installer."
-      sudo /usr/sbin/installer -pkg X950_Series_Print_Scan.pkg -target /
-      ppd_path="/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/PrintCore.framework/Resources/Generic.ppd"
-      echo "Cleaning up installer."
-      rm  -rf X950_Series_Print_Scan.pkg
-      ;;
-    *) 
-      echo "Unsupported platform: ${kernel_name}"
-      exit 2
-      ;;
+    echo "Run Lexmark installer."
+    sudo /usr/sbin/installer -pkg X950_Series_Print_Scan.pkg -target /
+    ppd_path="/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/PrintCore.framework/Resources/Generic.ppd"
+    echo "Cleaning up installer."
+    rm -rf X950_Series_Print_Scan.pkg
+    ;;
+  *)
+    echo "Unsupported platform: ${kernel_name}"
+    exit 2
+    ;;
   esac
 
-  echo " # Adding NAV Printer to CUPS."
-  sudo /usr/sbin/lpadmin -E -p "NAV_SikkerPrint" \
-    -v "smb://10.88.0.21/FargeDuplex%20IKSS" \
-    -P "${ppd_path}" \
-    -o printer-is-shared=false \
-    -o APOptionalDuplexer=True \
-    -o auth-info-required=username,password
+  case "${kernel_name}" in
+  Linux*)
+    echo " # Adding NAV Printer to CUPS."
+    sudo /usr/sbin/lpadmin -E -p "NAV_SikkerPrint" \
+      -v "smb://10.88.0.21/FargeDuplex%20IKSS" \
+      -P "${ppd_path}" \
+      -o printer-is-shared=false \
+      -o APOptionalDuplexer=True \
+      -o auth-info-required=username,password
+    ;;
+  Darwin*)
+    echo " # Adding NAV Printer to CUPS."
+    sudo /usr/sbin/lpadmin -E -p "NAV_SikkerPrint" \
+      -v "smb://A01PSVW005.adeo.no/MacUtskrift?encryption=no" \
+      -P "${ppd_path}" \
+      -o printer-is-shared=false \
+      -o APOptionalDuplexer=True \
+      -o auth-info-required=username,password
+    ;;
+  esac
 
   sudo /usr/sbin/cupsenable "NAV_SikkerPrint"
   sudo /usr/sbin/cupsaccept "NAV_SikkerPrint"
 
   case "${kernel_name}" in
-    Linux*)
-      echo "Restarting cups service"
-      sudo systemctl restart cups
-      ;;
+  Linux*)
+    echo "Restarting cups service"
+    sudo systemctl restart cups
+    ;;
   esac
 
   echo "# Done!"
